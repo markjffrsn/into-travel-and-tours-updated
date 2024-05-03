b7628f67e0819b87826f641c5b4ecd4c949333cd
markjffrsn <markjeffersonhidalgo08@gmail.com>
2024-05-03 22:33:53 +0800

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,17 @@
-import Images from './Images';
-import Icons from './Icons';
-import { Link } from 'react-router-dom';
+import Images from "./Images";
+import Icons from "./Icons";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
-	return (
-		<section className="hero">
-			<div
-				id="hero-carousel"
-				className="carousel slide bg-black"
-				data-bs-ride="carousel"
-				data-bs-interval="5000"
-			>
-				{/* <div className="carousel-indicators">
+  return (
+    <section className="hero">
+      <div
+        id="hero-carousel"
+        className="carousel slide bg-black"
+        data-bs-ride="carousel"
+        data-bs-interval="5000"
+      >
+        {/* <div className="carousel-indicators">
 						<button
 							type="button"
 							data-bs-target="#hero-carousel"
@@ -40,63 +40,63 @@ export default function Hero() {
 						></button>
 					</div> */}
 
-				<div className="carousel-inner">
-					<div className="carousel-item active">
-						<img src={Images.bgHome} className="d-block w-100" loading="lazy" />
-					</div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={Images.bgHome} className="d-block w-100" loading="lazy" />
+          </div>
 
-					<div className="carousel-item">
-						<img
-							src={Images.bgAbout1}
-							className="d-block w-100"
-							loading="lazy"
-						/>
-					</div>
+          <div className="carousel-item">
+            <img
+              src={Images.bgAbout1}
+              className="d-block w-100"
+              loading="lazy"
+            />
+          </div>
 
-					<div className="carousel-item">
-						<img
-							src={Images.bgHero2}
-							className="d-block w-100"
-							loading="lazy"
-						/>
-					</div>
+          <div className="carousel-item">
+            <img
+              src={Images.bgHero2}
+              className="d-block w-100"
+              loading="lazy"
+            />
+          </div>
 
-					<div className="carousel-item">
-						<img src={Images.bgHero} className="d-block w-100" loading="lazy" />
-					</div>
-				</div>
+          <div className="carousel-item">
+            <img src={Images.bgHero} className="d-block w-100" loading="lazy" />
+          </div>
+        </div>
 
-				<div className="hero-elements">
-					<h2>"ALWAYS AT ITS BEST!"</h2>
-					<h1>Let Us Take You on an Extraordinary Adventure!</h1>
-					<Link to="/getintouch">Book A Tour Now</Link>
-					<br />
-					{/* <a href="#exploreMore" className="scroll-down"></a> */}
-				</div>
-			</div>
+        <div className="hero-elements">
+          <h2>"ALWAYS AT ITS BEST!"</h2>
+          <h1>Let Us Take You on an Extraordinary Adventure!</h1>
+          <Link to="/getintouch">Book A Tour Now</Link>
+          <br />
+          {/* <a href="#exploreMore" className="scroll-down"></a> */}
+        </div>
+      </div>
 
-			<button
-				className="carousel-control-prev hero-prev-btn"
-				type="button"
-				data-bs-target="#hero-carousel"
-				data-bs-slide="prev"
-			>
-				<span className="carousel-slider p-2" aria-hidden="true">
-					{Icons.chevronLeft}
-				</span>
-				<span className="visually-hidden">Previous</span>
-			</button>
-			<button
-				className="carousel-control-next hero-next-btn"
-				type="button"
-				data-bs-target="#hero-carousel"
-				data-bs-slide="next"
-			>
-				<span className="carousel-slider p-2" aria-hidden="true">
-					{Icons.chevronRight}
-				</span>
-				<span className="visually-hidden">Next</span>
-			</button>
-		</section>
-	);
+      <button
+        className="carousel-control-prev hero-prev-btn"
+        type="button"
+        data-bs-target="#hero-carousel"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-slider p-2" aria-hidden="true">
+          {Icons.chevronLeft}
+        </span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next hero-next-btn"
+        type="button"
+        data-bs-target="#hero-carousel"
+        data-bs-slide="next"
+      >
+        <span className="carousel-slider p-2" aria-hidden="true">
+          {Icons.chevronRight}
+        </span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </section>
+  );
 }
